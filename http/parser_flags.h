1f59da15ae28ea3c9fa1e1732b1c873003b62ab0
snnn119@gmail.com <snnn119@gmail.com@1e8a0401-2685-3df8-1f11-f1d539f8b122>
2012-01-30 04:03:04 +0000

    Connection支持upgrade。

--- http/parser_flags.h
+++ http/parser_flags.h
@@ -7,4 +7,5 @@ enum parser_flags {
     parser_flag_connection_keep_alive = 1 << 1,
     parser_flag_connection_close      = 1 << 2,
     parser_flag_trailing              = 1 << 3,
+	parser_flag_connection_upgrade    = 1 << 4,	
 };
